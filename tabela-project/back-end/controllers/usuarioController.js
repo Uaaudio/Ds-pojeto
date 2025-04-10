2f82cbac6ea7ab88d530aa6a4874a6893d5c9b61
Pedro Oliveira <pedro.otavioo9@icloud.com>
2025-04-10 08:41:05 -0300

    adicionando logica async/await para controller

--- tabela-project/back-end/controllers/usuarioController.js
+++ tabela-project/back-end/controllers/usuarioController.js
@@ -3,39 +3,42 @@ const bcrypt = require ('bcrypt');
 
 // funcao de cadastro
 
-exports.cadastrarUsuario = (req,res) => {
-    const {nome, email, senha, nivel_acesso} =  req.body;
-
+exports.cadastrarUsuario = async (req,res) => {
+    try{    const {nome, email, senha, nivel_acesso} =  req.body;
     //verificar se todos os campos foram preenchidos
     
-    if(!nome || !email || !senha || !nivel_acesso){
-        return res.status(400).json({error: 'Todos os campos são obrigatórios!'})
-    }
-
-    // verificar se o email já existe no banco de dados
-
-    usuarioModel.buscarPorEmail(email, (err, result) => {
-        if (err) {
-            return res.status(500).json({error: 'Erro ao verificar o e-mail:'});
-        }
-        if (result.length > 0){
-            return res.status(400).json({error: 'Este e-mail já foi cadastrado!'});
-        }
-
-    // criptografar a senha antes de salvar no banco
-    bcrypt.hash(senha, 10, (err,hashedPassword) => {
-        if (err){
-            return res.status(500).json({error: 'Erro ao criptografar senha.'});
-        }
-    // inserir o novo usuário no banco de dados
-    usuarioModel.cadastrar(nome, email, hashedPassword, nivel_acesso,(err, result) => {
-        if (err){
-            console.error('Erro ao cadastrar o usuário:', err) // exibe o erro detalhado
-            return res.status(500).json({error: 'Erro ao cadastrar usuário.'});
-        }
-        
-        res.status(201).json({message: 'Usuário cadastrado com sucesso!'});
-    });
-});
-});
+     if(!nome || !email || !senha || !nivel_acesso){
+    return res.status(400).json({error: 'Todos os campos são obrigatórios!'})
 }
+    // verificar se o email já existe no banco de dados
+    const usuarioExistente = await usuarioModel.buscarPorEmail(email);
+    if (usuarioExistente){ return res.status(400).json({error: 'Este e-mail já foi cadastrado!'});
+}
+    // criptografar a senha
+    const hash = await bcrypt.hash(senha,10)
+
+    // cadastrar usuario
+    const resultado = await usuarioModel.cadastrar(
+        nome,
+        email,
+        hash,
+        nivel_acesso
+    )
+    // resposta suscesso
+    res.status(201).json({
+        message: 'Usuario cadastrado com sucesso!',
+        userId: resultado.insertId
+    });
+}catch (error){
+    console.error('Erro no cadastro:', error);
+    //tratamento especifico para erros conhecidos
+    if (error.message === 'Este email ja esta cadastrado'){
+        return res.status(400).json({error: error.message});
+    }
+    //erro generico
+    res.status(500).json({
+        error: 'Erro interno no servidor',
+        details: process.env.NODE_ENV === 'development' ? error.message: undefined
+    });
+} 
+};
